a1b8b8c0f50bac3f396f68f633c55319b8ade9e6
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2026-03-24 09:48:28 -0300

    Add files via upload

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import Sidebar from './Sidebar';
 import Header from './Header';
 
 interface AppLayoutProps {
-  children:   React.ReactNode;
-  title:      string;
-  subtitle?:  string;
-  actions?:   React.ReactNode;
+  children:  React.ReactNode;
+  title:     string;
+  subtitle?: string;
+  actions?:  React.ReactNode;
 }
 
 export default function AppLayout({ children, title, subtitle, actions }: AppLayoutProps) {
@@ -14,7 +16,7 @@ export default function AppLayout({ children, title, subtitle, actions }: AppLay
       <Sidebar />
 
       <div className="flex-1 ml-[240px] flex flex-col min-h-screen">
-        <Header title={title} breadcrumb={subtitle} actions={actions} />
+        <Header title={title} subtitle={subtitle} actions={actions} />
         <main className="flex-1 p-7">
           {children}
         </main>
